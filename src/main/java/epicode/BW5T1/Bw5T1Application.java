9bcf348001c3a3cc4c0e638bc849c6928c910652
Gabriele Alleruzzo <alleruzzo.gabriele.96@gmail.com>
2025-06-23 16:22:47 +0200

    Push main/master

---
+++ src/main/java/epicode/BW5T1/Bw5T1Application.java
@@ -0,0 +1,13 @@
+package epicode.BW5T1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bw5T1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bw5T1Application.class, args);
+	}
+
+}
---
+++ src/test/java/epicode/BW5T1/Bw5T1ApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.BW5T1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bw5T1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
