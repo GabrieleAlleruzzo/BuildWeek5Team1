d7d42678168acf81af2b3ac9cb90e2f75074d6d8
Barbara <barbyns95@gmail.com>
2025-06-24 11:18:09 +0200

    up

--- src/main/java/epicode/BW5T1/model/Indirizzo.java
+++ src/main/java/epicode/BW5T1/model/Indirizzo.java
@@ -1,4 +1,26 @@
 package epicode.BW5T1.model;
 
+
+import entities.Comune;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.security.core.userdetails.User;
+@Data
+@Entity
 public class Indirizzo {
+    @Id @GeneratedValue
+    private Long id;
+
+    private String via;
+    private String civico;
+    private String localita;
+    private String cap;
+
+    @ManyToOne
+    private Comune comune;
+
+
 }
