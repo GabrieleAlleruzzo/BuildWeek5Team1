feace7e5b32e8672f1e11b1bbc3e5275d02b1279
MaddalenaRen <ileniarennella@gmail.com>
2025-06-26 17:33:04 +0200

    metodi aggiornati

---
+++ src/main/java/epicode/BW5T1/enumeration/TipoIndirizzo.java
@@ -0,0 +1,6 @@
+package epicode.BW5T1.enumeration;
+
+public enum TipoIndirizzo {
+    SEDE_LEGALE,
+    SEDE_OPERATIVA
+}
--- src/main/java/epicode/BW5T1/model/Provincia.java
+++ src/main/java/epicode/BW5T1/model/Provincia.java
@@ -1,5 +1,6 @@
 package epicode.BW5T1.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -10,6 +11,7 @@ import java.util.Set;
 
 @Entity
 @Data
+@ToString(exclude = "comuni")
 public class Provincia {
     @Id
     @GeneratedValue
@@ -18,6 +20,7 @@ public class Provincia {
     private String nome;
     private String sigla;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "provincia")
     private Set<Comune> comuni ;
 
