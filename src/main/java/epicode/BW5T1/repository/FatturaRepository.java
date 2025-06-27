e2293661c460c8d58735563067e0caae18055568
Silvia <silvia8gasparini@gmail.com>
2025-06-27 14:57:10 +0200

    metodi mancanti Fattura

--- src/main/java/epicode/BW5T1/repository/FatturaRepository.java
+++ src/main/java/epicode/BW5T1/repository/FatturaRepository.java
@@ -1,12 +1,26 @@
 package epicode.BW5T1.repository;
 
 import epicode.BW5T1.model.Fattura;
-import org.springframework.data.jpa.repository.JpaRepository;
+import epicode.BW5T1.enumeration.StatoFattura;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
 
 public interface FatturaRepository extends JpaRepository<Fattura, Integer> {
 
     boolean existsByNumeroAndClienteId(String numero, int clienteId);
 
+    Page<Fattura> findByClienteId(int clienteId, Pageable pageable);
 
+    Page<Fattura> findByStatoFattura(StatoFattura statoFattura, Pageable pageable);
 
+    Page<Fattura> findByData(LocalDate data, Pageable pageable);
+
+    @Query("SELECT f FROM Fattura f WHERE YEAR(f.data) = :anno")
+    Page<Fattura> findByAnno(@Param("anno") int anno, Pageable pageable);
+
+    Page<Fattura> findByImportoBetween(double min, double max, Pageable pageable);
 }
