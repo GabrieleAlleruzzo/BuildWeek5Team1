2f7456473c13e74820573e4501d82f973e30a6d1
Roberto Albergo <ralbergo7@gmail.com>
2025-06-27 15:20:21 +0200

    Update FatturaRepository.java

--- src/main/java/epicode/BW5T1/repository/FatturaRepository.java
+++ src/main/java/epicode/BW5T1/repository/FatturaRepository.java
@@ -1,11 +1,16 @@
 package epicode.BW5T1.repository;
 
+import epicode.BW5T1.enumeration.StatoFattura;
 import epicode.BW5T1.model.Fattura;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface FatturaRepository extends JpaRepository<Fattura, Integer> {
 
     boolean existsByNumeroAndClienteId(String numero, int clienteId);
+
 
 
 
